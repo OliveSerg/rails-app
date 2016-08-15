bfcef5e10182fe0884081121a2c001ad91f91433
Sergio <oliveserg@gmail.com>
2016-08-15 16:55:46 +0000

    resources routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,37 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update_attributes(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  protected
+
+  def post_params
+    params.require(:post).permit(:title, :author, :text)
+  end
 end
